ccde6879c1a3c7c5c04d84ad0411fa94c59786a1
Budderstock <shanejanish@gmail.com>
2022-09-13 09:55:01 -0500

    Shane Anish - Variable Lab

---
+++ src/Variables.java
@@ -0,0 +1,33 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte a = 127;
+        short b = 20000;
+        int c = 39247292;
+        long d = 320000000000L;
+        float e = 1.9F;
+        double f = 9.51937194;
+        char g = '$';
+        boolean h = true;
+        String i = "Tianbo Zhang says to keep his shirt on.";
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
